f0a88000b547842b922f36361df71b9c7c52f220
Ricardo Nunes <ricardo.nunes@coriant.com>
2018-01-05 22:09:37 +0000

    New classes

---
+++ src/com/ossnms.sandbox/interview/RemoveDups.java
@@ -0,0 +1,45 @@
+package com.ossnms.sandbox.interview;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.util.LinkedList;
+
+public class RemoveDups {
+
+    public static void main(String[] args) {
+        new RemoveDups().go();
+    }
+
+    private void go() {
+        LinkedList<Integer> linkedList = LinkedList.fill(Arrays.asList(1, 1, 2, 3, 3, 5, 6, 7, 7));
+        LinkedList<Integer> noDups = removeDups(linkedList);
+        System.out.println(noDups);
+    }
+
+    private <T> LinkedList<T> removeDups(LinkedList<T> linkedList) {
+        Set<T> foundSet = new HashSet<>();
+
+        LinkedList.LinkedListNode<T> node = linkedList.getHead();
+
+        while(node != null) {
+            if(!foundSet.add(node.getObject())) {
+                LinkedList.LinkedListNode<T> previous = node.getPrevious();
+                LinkedList.LinkedListNode<T> next = node.getNext();
+
+                if (previous != null) {
+                    previous.setNext(next);
+                }
+
+                if (next != null) {
+                    next.setPrevious(previous);
+                }
+            }
+            node = node.getNext();
+        }
+
+        return linkedList;
+    }
+
+}
